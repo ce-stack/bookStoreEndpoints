4a91c0d5516ca70cb363772858d9e99e1541db1c
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-04-25 22:16:05 +0300

    add AuthService repos impl

--- src/main/java/com/example/demo/services/AuthService.java
+++ src/main/java/com/example/demo/services/AuthService.java
@@ -3,10 +3,23 @@ package com.example.demo.services;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.models.User;
+import com.example.demo.repositories.RoleRepository;
+import com.example.demo.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public AuthService(UserRepository userRepository , RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+    }
+
     public AuthResponse login(LoginRequest request) {
         return new AuthResponse("dummy-token" , request.getEmail() , "amirmohammed");
     }
