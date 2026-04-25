4a696e735344f412d46f34246b2fa9717177e5a3
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-04-25 15:01:21 +0300

    add the auth response and service login and register

--- src/main/java/com/example/demo/services/AuthService.java
+++ src/main/java/com/example/demo/services/AuthService.java
@@ -1,4 +1,17 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    public AuthResponse login(LoginRequest request) {
+        return new AuthResponse("dummy-token" , request.getEmail() , "amirmohammed");
+    }
+
+    public AuthResponse register(RegisterRequest $request){
+        return  new AuthResponse("dummy-token" , $request.getEmail(), "amiremohammed");
+    }
 }
