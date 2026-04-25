a4d24e5e3c62e6d9de8e32763651e579df2c299f
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-04-25 07:40:18 +0300

    add permission

---
+++ src/main/java/models/Permission.java
@@ -0,0 +1,59 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "permissions")
+public class Permission {
+
+    private int id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+
+    public Permission() {
+
+    }
+
+    public Permission(String name, Role role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Permission{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
