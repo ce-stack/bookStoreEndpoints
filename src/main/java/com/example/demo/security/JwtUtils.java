8547058f53826c8684b70bbe4b8627230c56f49c
Amir Mohammed <32426843+ce-stack@users.noreply.github.com>
2026-04-27 02:08:48 +0300

    add the jwt class and package

---
+++ src/main/java/com/example/demo/security/JwtUtils.java
@@ -0,0 +1,49 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExperation;
+
+    public String generateJwtToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration( new Date(( new Date()).getTime() + jwtExperation))
+                .signWith(SignatureAlgorithm.HS256 , jwtSecret)
+                .compact();
+    }
+
+    public Claims getClaimsFormToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            getClaimsFormToken(authToken);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return getClaimsFormToken(token).getSubject();
+    }
+
+}
